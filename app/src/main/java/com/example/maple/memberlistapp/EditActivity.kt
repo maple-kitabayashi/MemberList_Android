756bc5d35cbf274799519c0eb41de91757949bd3
maple <maple@maplenoiMac.local>
2018-06-12 15:10:06 +0900

    EditActivityを追加

---
+++ app/src/main/java/com/example/maple/memberlistapp/EditActivity.kt
@@ -0,0 +1,12 @@
+package com.example.maple.memberlistapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class EditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
