ff6be6dd2f86ff6424057be102700a5df5146284
maple-kitabayashi <junki.kitabayashi@maplesystems.co.jp>
2018-06-13 16:51:15 +0900

    LoginActivity作成

---
+++ app/src/main/java/com/example/maple/memberlistapp/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.example.maple.memberlistapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+
+class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = LoginActivity::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+}
