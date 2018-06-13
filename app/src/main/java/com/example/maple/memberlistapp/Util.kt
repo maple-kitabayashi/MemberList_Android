cccd883aa53e9dae05abfe457b1730b138647f21
maple-kitabayashi <junki.kitabayashi@maplesystems.co.jp>
2018-06-13 12:17:22 +0900

    Utilクラス作成、最終更新日のデータを挿入

---
+++ app/src/main/java/com/example/maple/memberlistapp/Util.kt
@@ -0,0 +1,9 @@
+package com.example.maple.memberlistapp
+
+class Util {
+    companion object {
+        val PREF_LAST_UPDATE: String = "prefLastUpdate"
+        val DEFAULT_LAST_UPDATE_TIME: String = "1990-01-01 00:00:00" //最終更新日
+        val PREF_KEY_LAST_UPDATE_TIME: String = "lastUpdateTimeKey"
+    }
+}
