6c510e36a56ed7b40148ffe56d962651522aa118
maple-kitabayashi <junki.kitabayashi@maplesystems.co.jp>
2018-06-13 10:35:36 +0900

    新APIからデータ取得可能なのを確認(未対応)

--- app/src/main/java/com/example/maple/memberlistapp/UserService.kt
+++ app/src/main/java/com/example/maple/memberlistapp/UserService.kt
@@ -1,19 +1,15 @@
 package com.example.maple.memberlistapp
 
 import com.example.maple.memberlistapp.api.User
-import retrofit2.http.GET
 import io.reactivex.Observable
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.Headers
+import retrofit2.http.*
 
 interface UserService {
 
     //ユーザデータを取得
-    @FormUrlEncoded
     @Headers("Authorization: Token GfjrNvFaqj6Ebhda")
     @GET("api/v1/employees")
     fun getUsers(
-            @Field("lastUpDate") lastUpDate: String //最終更新日
+            @Query("lastUpDate") lastUpDate: String //最終更新日
     ): Observable<List<User>>
 }
