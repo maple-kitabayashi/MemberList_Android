966cc8d10e6fbfe32497af111eb3fcddf2266261
maple-kitabayashi <junki.kitabayashi@maplesystems.co.jp>
2018-06-21 15:27:45 +0900

    const追記

--- app/src/main/java/com/example/maple/memberlistapp/Util.kt
+++ app/src/main/java/com/example/maple/memberlistapp/Util.kt
@@ -4,13 +4,13 @@ import android.content.SharedPreferences
 
 class Util {
     companion object {
-        val PREF_LAST_UPDATE:          String = "prefLastUpdate"
-        val DEFAULT_LAST_UPDATE_TIME:  String = "1990-01-01 00:00:00" //最終更新日
-        val PREF_KEY_LAST_UPDATE_TIME: String = "lastUpdateTimeKey"
+        const val PREF_LAST_UPDATE:          String = "prefLastUpdate"
+        const val DEFAULT_LAST_UPDATE_TIME:  String = "1990-01-01 00:00:00" //最終更新日
+        const val PREF_KEY_LAST_UPDATE_TIME: String = "lastUpdateTimeKey"
 
-        val PREF_LOGIN_STATUS:         String  = "prefLoginStatus"
-        val DEFAULT_LOGIN_STATUS:      Boolean = false
-        val PREF_KEY_LOGIN_STATUS:     String  = "loginStatusKey"
+        const val PREF_LOGIN_STATUS:         String  = "prefLoginStatus"
+        const val DEFAULT_LOGIN_STATUS:      Boolean = false
+        const val PREF_KEY_LOGIN_STATUS:     String  = "loginStatusKey"
     }
 
 
