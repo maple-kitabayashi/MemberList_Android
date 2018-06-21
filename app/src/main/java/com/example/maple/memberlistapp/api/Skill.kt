c981ccb776bc4704dfe939d4743e81febb7f823b
maple-kitabayashi <junki.kitabayashi@maplesystems.co.jp>
2018-06-21 13:44:50 +0900

    キーワード変更

--- app/src/main/java/com/example/maple/memberlistapp/api/Skill.kt
+++ app/src/main/java/com/example/maple/memberlistapp/api/Skill.kt
@@ -5,8 +5,8 @@ import io.realm.annotations.PrimaryKey
 import io.realm.annotations.Required
 
 open class Skill: RealmObject() {
-    @PrimaryKey val id:         String = "" //ID
-    @Required   val skill_name: String = "" //スキル名
-    @Required   val created_at: String = "" //作成日時
-    @Required   val updated_at: String = "" //更新日時
+    @PrimaryKey var id:         String = "" //ID
+    @Required   var skill_name: String = "" //スキル名
+    @Required   var created_at: String = "" //作成日時
+    @Required   var updated_at: String = "" //更新日時
 }
